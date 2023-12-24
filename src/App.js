04ae38aa59f6b3aa249e31a2a206c5404a6101b3
hassan <hassanak509@gmail.com>
2023-12-24 22:36:28 +0200

    some changes was made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import ButtonsPart from './components/ButtonsPart';
+import Line from './components/Line';
+import ResultScreen from './components/ResultScreen';
+import Screen from './components/Screen';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Screen>
+        <ResultScreen />
+        <ButtonsPart />
+        <Line />
+      </Screen>
     </div>
   );
 }
