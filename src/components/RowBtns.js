17f215a09e247b519b3921545a6e1a548aa7749b
hassan <hassanak509@gmail.com>
2023-12-26 22:20:40 +0200

    add a component called RowBtns

---
+++ src/components/RowBtns.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+export default function RowBtns() {
+  return (
+    <div className='row-btns'>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  )
+}
