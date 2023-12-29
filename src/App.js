7ab7d0e6b5608657c08c9aa3bd6e0df24e87a7ab
hassan <hassanak509@gmail.com>
2023-12-29 18:56:37 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import ButtonsPart from './components/ButtonsPart';
-// import Line from './components/Line';
+import Line from './components/Line';
 import ResultScreen from './components/ResultScreen';
 import Screen from './components/Screen';
+import { ValuesProvider } from './context/valuesContext';
 import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Screen>
-        <ResultScreen />
-        <ButtonsPart />
-        {/* <Line /> */}
-      </Screen>
-    </div>
+    <ValuesProvider>
+      <div className="App">
+        <Screen>
+          <ResultScreen />
+          <ButtonsPart />
+          <Line />
+        </Screen>
+      </div>
+    </ValuesProvider>
   );
 }
 
