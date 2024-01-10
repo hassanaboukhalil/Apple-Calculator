0aba1c119e6dcbfed6eb937cdab43c83afbd335b
hassan <hassanak509@gmail.com>
2024-01-10 10:31:17 +0200

    add some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+// import App from './App';
+import ResultScreen from './components/ResultScreen';
+import { ValuesProvider } from './context/valuesContext';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders the output on the screen', () => {
+  // Render the component
+  render(
+    <ValuesProvider>
+      <ResultScreen />
+    </ValuesProvider>
+  );
+
+  // Get the element by ID
+  const res = screen.getByTestId('result');
+
+  // Check the innerHTML
+  expect(res).not.toHaveTextContent('');
+
+});
+
+test('the output number should not contain more than one point/dot', () => {
+  // Render the component
+  render(
+    <ValuesProvider>
+      <ResultScreen />
+    </ValuesProvider>
+  );
+
+  // Get the element by ID
+  let nb = screen.getByTestId('result').textContent;
+
+  // Check the innerHTML
+  expect(Number(nb)).not.toEqual(NaN);
+
+});
